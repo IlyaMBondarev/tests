c082f59dad9fd518c1614d1381760db4d6a8dbc0
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2020-10-10 13:19:26 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,26 +2,6 @@
 document.querySelector('.wrapper').classList.add('loaded');
 
 
-let dropBtns = document.querySelectorAll('.drop');
-
-dropBtns.forEach(button => {
-    let dropMenu = button.querySelector('.drop-menu');
-    button.addEventListener('click', () => {
-        dropMenu.classList.toggle('dropped');
-    })
-})
-
-document.addEventListener('click', (event) => {
-    let target = event.target;
-    dropBtns.forEach(button => {
-        if (target !== button || !(button.contains(target))) {
-            let droppedMenu = button.querySelector('.dropped');
-            if (droppedMenu) {
-                droppedMenu.classList.remove('dropped');
-            }
-        }
-    })
-})
 let sliders = document.querySelectorAll('.slider');
 
 for (let i = 0; i < sliders.length; i++) {
