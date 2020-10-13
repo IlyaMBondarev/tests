8968e672ea89877ba8a05033126dfcba423914e5
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2020-10-13 12:46:42 +0300

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -1,182 +1,6 @@
 
-document.querySelector('.wrapper').classList.add('loaded');
-
-
-let list = document.querySelector("body"), dir, i, isOkToScroll, target;
-let scale = document.querySelector('.scale');
-let setOfImages = document.querySelector('.set-of-images');
-setDefault();
-
-scale.addEventListener("wheel", onWheel);
-setOfImages.addEventListener("wheel", onWheel);
-
-function onWheel(event) {
-    event.preventDefault();
-    dir = event.deltaY > 0 ? 1 : -1;
-    isOkToScroll = true;
-    target = this;
-}
-
-requestAnimationFrame(scroll);
-
-function scroll() {
-    requestAnimationFrame(scroll)
-    if (!isOkToScroll) return;
-
-    if (i < 28) i++
-    else setDefault();
-
-    if (target === setOfImages) {
-        target.scrollBy(2 * i * dir, 0);
-    } else {
-        window.scrollBy(2 * i * dir, 0);
-    }
-}
-
-function setDefault() {
-    isOkToScroll = false;
-    i = 1;
-    dir = 0;
-}
-
-let isOkToMove, xCoordinate, yCoordinate, xCoordinateChanged, yCoordinateChanged;
-
-scale.addEventListener('mousedown', (event) => {
-    if (!(event.target.draggable)) {
-        isOkToMove = true;
-        xCoordinate = event.clientX;
-        yCoordinate = event.clientY;
-    }
-})
-
-document.addEventListener('mousemove', (event) => {
-    if (isOkToMove) {
-        xCoordinateChanged = event.clientX;
-        yCoordinateChanged = event.clientY;
-        window.scrollBy(xCoordinate - xCoordinateChanged, yCoordinate - yCoordinateChanged);
-        xCoordinate = xCoordinateChanged;
-        yCoordinate = yCoordinateChanged;
-
-    }
-})
-
-document.addEventListener('mouseup', (event) => {
-    isOkToMove = false;
-})
-
-
-// первоначальные места картинок
-let imageSpawns = document.querySelectorAll('.image-spawn');
-let imageSpawnsSmall = document.querySelectorAll('.image-spawn-small');
-let imageSpawnsLarge = document.querySelectorAll('.image-spawn-large');
-
-// баллы
 let score = document.querySelector('.score');
 score.textContent = '0';
-
-let countScale = 0;
-let indexOfQuestion = 0;
-
-// иициализация переменной, в которой будут храниться картинки
-let images = [];
-let imagesSmall = [];
-let imagesLarge = [];
-
-//заполняем картинки
-imageSpawns.forEach(spawn => {
-    images.push(spawn.querySelector('img'));
-});
-imageSpawnsSmall.forEach(spawn => imagesSmall.push(spawn.querySelector('img')));
-imageSpawnsLarge.forEach(spawn => imagesLarge.push(spawn.querySelector('img')));
-
-
-//картинка начинает движение
-var dragged;
-
-/* events fired on the draggable target */
-document.addEventListener("drag", function(event) {
-
-}, false);
-
-document.addEventListener("dragstart", function(event) {
-    // store a ref. on the dragged elem
-    dragged = event.target;
-}, false);
-
-document.addEventListener("dragend", function(event) {
-    // reset the transparency
-    event.target.style.opacity = "";
-}, false);
-
-/* events fired on the drop targets */
-document.addEventListener("dragover", function(event) {
-    // prevent default to allow drop
-    event.preventDefault();
-}, false);
-
-document.addEventListener("dragenter", function(event) {
-    // highlight potential drop target when the draggable element enters it
-    if (event.target.classList.contains("place")) {
-
-    }
-
-}, false);
-
-document.addEventListener("dragleave", function(event) {
-    // reset background of potential drop target when the draggable element leaves it
-    if (event.target.classList.contains("place")) {
-
-    }
-
-}, false);
-
-document.addEventListener("drop", function(event) {
-    // prevent default action (open as link for some elements)
-    event.preventDefault();
-    // move dragged elem to the selected drop target
-    if (event.target.classList.contains("place") && event.target.classList.contains("empty") && event.target.dataset.place === dragged.dataset.place) {
-        if (dragged.parentNode.classList.contains('image-spawn')) {
-            dragged.parentNode.style.width = '0px';
-            dragged.parentNode.style.margin = '0px';
-        } else {
-            dragged.parentNode.classList.add('empty');
-        }
-        event.target.classList.remove('empty');
-        dragged.parentNode.removeChild(dragged);
-        event.target.appendChild(dragged);
-        if (dragged.dataset.answer === dragged.parentNode.dataset.answer) {
-            score.textContent = `${+(score.textContent) + 2 - dragged.dataset.penalty}`;
-            dragged.draggable = false;
-            countScale++;
-            if (countScale === images.length) {
-                scale.style.display = "none";
-                setOfImages.style.display = "none";
-                question(questions[indexOfQuestion]);
-            }
-        } else {
-            dragged.dataset.penalty = '2';
-            score.textContent = `${+(score.textContent) - 1}`;
-        }
-    } else if (!(event.target.classList.contains('place')) && !(event.target.parentNode.classList.contains('place'))) {
-        dragged.parentNode.classList.add('empty');
-        dragged.parentNode.removeChild(dragged);
-        imageSpawns[+(dragged.dataset.startplace) - 1].style.width = '';
-        imageSpawns[+(dragged.dataset.startplace) - 1].style.margin = '';
-        imageSpawns[+(dragged.dataset.startplace) - 1].appendChild(dragged);
-    }
-}, false);
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 let typeOfAnswer = "";
@@ -193,7 +17,6 @@ let quizAnswer = quiz.querySelector('.quiz__answer-block');
 let quizRightAnswer = quiz.querySelector('.quiz__right-answer-block');
 let quizSubmit = quiz.querySelector('.quiz__submit');
 let quizNext = quiz.querySelector('.quiz__next');
-let quizNextPage = quiz.querySelector('.quiz__next-page');
 
 let quizAnswerTextBlock = quizAnswer.querySelector('.quiz__answer');
 let quizAnswerText = quizAnswer.querySelector('.quiz__answer-text');
@@ -332,7 +155,7 @@ quizSubmit.addEventListener('click', () => {
         }
     } else if (typeOfAnswer === "table-horizontal") {
         let answerTable = "";
-        let answerInputs = quizAnswerTableHorizontal.querySelectorAll('input');
+        let answerInputs = quizAnswerTableHorizontal.querySelectorAll('.quiz__answer-number');
         answerInputs.forEach(input => answerTable += input.value);
         let rightAnswerTable = "";
         let rightAnswerSpans = quizRightAnswerTableHorizontal.querySelectorAll('span');
@@ -349,10 +172,10 @@ quizSubmit.addEventListener('click', () => {
         }
     } else if (typeOfAnswer === "table-vertical") {
         let answerTable = "";
-        let answerInputs = quizAnswerTableVertical.querySelectorAll('input');
+        let answerInputs = quizAnswerTableHorizontal.querySelectorAll('.quiz__answer-number');
         answerInputs.forEach(input => answerTable += input.value);
         let rightAnswerTable = "";
-        let rightAnswerSpans = quizRightAnswerTableVertical.querySelectorAll('span');
+        let rightAnswerSpans = quizRightAnswerTableHorizontal.querySelectorAll('span');
         rightAnswerSpans.forEach(span => rightAnswerTable += span.textContent);
         if (answerTable.toLowerCase() === rightAnswerTable.toLowerCase()) {
             if (!isHelperUsed) {
@@ -369,16 +192,16 @@ quizSubmit.addEventListener('click', () => {
     } else {
         console.error('Тип вопроса неправильный');
     }
-    if (indexOfQuestion !== questions.length - 1) {
-        quizNext.style.display = "table";
+    if (indexOfQuestion !== questions.length) {
+        quizNext.style.display = "block";
     } else {
-        quizNextPage.style.display = "table";
+        quizLinkToNextPage.style.display = "block";
     }
 })
 
 quizNext.addEventListener('click', () => {
     quizRightAnswer.style.display = "none";
-    quizSubmit.style.display = "table";
+    quizSubmit.style.display = "block";
     quizNext.style.display = "none";
     quiz.style.display = "none";
     helpers.style.display = "none";
@@ -409,6 +232,8 @@ quizNext.addEventListener('click', () => {
     }
 })
 
+let indexOfQuestion = 0;
+question(questions[indexOfQuestion]);
 
 
 
